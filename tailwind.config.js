ae9122e585174a2cf5fce4127611928674d70298
Linhdev29 <linhvd29@gmail.com>
2025-07-24 13:16:35 +0700

    feat/add mugs product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,24 +7,35 @@ export default {
         Karla: ["Karla", "sans-serif"]
       },
       colors: {
-        'coffee': {
+        coffee: {
           50: "#E8D6D0",
           200: "#C89f94",
           400: "#A25F4B",
-          600: "#744838",
+          600: "#744838"
         }
       },
       keyframes: {
         sliderDown: {
           "0%": { transform: "translateY(-100%)" },
           "100%": { transform: "translateY(0)" }
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 }
         }
       },
       animation: {
-        sliderDown: "sliderDown 0.4s ease-in-out"
+        sliderDown: "sliderDown 0.5s ease-in-out",
+        fadeIn: "fadeIn 0.3s ease-in-out"
       },
       backgroundImage: {
-        'slider-bg': "url('./images/slider-coffee.jpg')"
+        "slider-bg": "url('./images/slider-coffee.jpg')",
+        ...Object.fromEntries(
+          Array.from({ length: 5 }, (_, i) => [
+            `product-bg-${i + 1}`,
+            `url('./images/feat-mug-${i + 1}.jpg')`
+          ])
+        )
       }
     }
   },
